5fda32252424c3614329f4ca77329c187671de54
Rob Solomon <rob@imtc.gatech.edu>
2014-06-10 12:48:05 -0400

    Adding Tyler's Prototype Project

---
+++ Experiments/labean-mousePrototype/Tyler Prototype/Assets/Scripts/Player/Old/SoundController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	public float maxVolume;
+	public float volumeScale;
+	public bool X, Y, Z, onlyGrounded;
+
+	private Vector3 oldPosition;
+	private AudioSource audioSource;
+
+	void Awake()
+	{
+		audioSource = GetComponent<AudioSource> ();
+		oldPosition = transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 delta = transform.position - oldPosition;
+		delta.x *= X ? 1 : 0;
+		delta.y *= Y ? 1 : 0;
+		delta.z *= Z ? 1 : 0;
+	}
+}
