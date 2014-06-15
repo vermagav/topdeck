c0c7bf11c879d34949fbaff342c69f76741a63e6
Gav Verma <verma.gav@gmail.com>
2014-06-15 15:21:44 -0400

    Created Deliverable 2 Project

---
+++ Deliverables/m2-physics/Assets/Scripts/Player/Controller Look/LookAtControllerYRotation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtControllerYRotation : MonoBehaviour {
+	
+	public float rotationSpeed;
+	public float lookAngle;
+
+	void FixedUpdate()
+	{
+		//Debug.Log (Input.GetAxis ("LookHorizontal"));
+
+		Vector3 point = new Vector3 (Input.GetAxis ("LookHorizontal"), 0f, Input.GetAxis ("LookVertical"));
+
+		Quaternion newLookRotation;
+
+		if(point.magnitude == 0f)
+		{
+			newLookRotation = transform.parent.rotation;
+		}
+		else
+		{
+			newLookRotation = Quaternion.LookRotation(point);
+		}
+
+		newLookRotation = Quaternion.RotateTowards(transform.parent.rotation, newLookRotation, lookAngle);
+		
+		transform.rotation = Quaternion.Lerp (transform.rotation, newLookRotation, rotationSpeed);
+	}
+}
---
+++ Deliverables/m2-physics/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (CapsuleCollider))]
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float maxVelocity;
+	public float maxForce;
+
+	private bool grounded = false;
+
+	void Awake () {
+		rigidbody.freezeRotation = true;
+	}
+	
+	void FixedUpdate () {
+
+		if (grounded) {
+			Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			if(targetVelocity.magnitude > 1)
+			{
+				targetVelocity = targetVelocity / targetVelocity.magnitude;
+			}
+
+			targetVelocity = transform.TransformDirection(targetVelocity);
+			targetVelocity *= maxVelocity;
+			
+			Vector3 force = Vector3.ClampMagnitude((targetVelocity - rigidbody.velocity)/rigidbody.mass, maxForce)/Time.deltaTime;
+
+			rigidbody.AddForce(force);
+		}
+
+		grounded = false;
+	}
+	
+	void OnCollisionStay () {
+		grounded = true;    
+	}
+
+}
