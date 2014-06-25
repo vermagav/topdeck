291439d943b701b3955b2c811fa77b351f5c5e5c
Gav Verma <verma.gav@gmail.com>
2014-06-25 14:52:19 -0400

    Removed old checks due to CrossFade()'s awesomeness

--- Deliverables/m2-physics/Assets/Scripts/Level/AnimateDoor.cs
+++ Deliverables/m2-physics/Assets/Scripts/Level/AnimateDoor.cs
@@ -6,25 +6,15 @@ public class AnimateDoor : MonoBehaviour {
 	private bool doorTriggered = false;
 
 	public void OpenDoor() {
-		// We only want the door to open
+		// We only want the door to open once per OnTriggerStay()
 		if (!doorTriggered) {
-			// Check for currently playing animation before playing
-			if(animation.IsPlaying("CloseDoor")) {
-				animation.CrossFade ("OpenDoor");
-			} else {
-				animation.Play ("OpenDoor");
-			}
+			animation.CrossFade("OpenDoor");
 			doorTriggered = true;
 		}
 	}
 
 	public void CloseDoor() {
-		// Same as in OpenDoor()
-		if(animation.IsPlaying("OpenDoor")) {
-			animation.CrossFade ("CloseDoor");
-		} else {
-			animation.Play ("CloseDoor");
-		}
+		animation.CrossFade ("CloseDoor");
 		doorTriggered = false;
 	}
 
