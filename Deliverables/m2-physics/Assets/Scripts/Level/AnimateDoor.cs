74fce5967ff7cb7ad0023298330c8acee84c0de4
Gav Verma <verma.gav@gmail.com>
2014-06-25 12:58:11 -0400

    Replaced door hinge with custom animations, trigger now animates door correctly

--- Deliverables/m2-physics/Assets/Scripts/Level/AnimateDoor.cs
+++ Deliverables/m2-physics/Assets/Scripts/Level/AnimateDoor.cs
@@ -3,15 +3,31 @@ using System.Collections;
 
 public class AnimateDoor : MonoBehaviour {
 
-	JointMotor motor = new JointMotor();
+	private bool doorTriggered = false;
 
 	public void OpenDoor() {
-		motor.force = 300;
-		motor.targetVelocity = 90;
-		hingeJoint.motor = this.motor;
+		// We only want the door to open
+		if (!doorTriggered) {
+			// Check for currently playing animation before queing
+			// This prevents the robot from indefinitely queing multiple animations
+			// by stepping on and off the button and using subsequent swings to escape
+			if(animation.IsPlaying("CloseDoor")) {
+				animation.PlayQueued ("OpenDoor");
+			} else {
+				animation.Play ("OpenDoor");
+			}
+			doorTriggered = true;
+		}
 	}
 
 	public void CloseDoor() {
+		// Same as in OpenDoor()
+		if(animation.IsPlaying("OpenDoor")) {
+			animation.PlayQueued("CloseDoor");
+		} else {
+			animation.PlayQueued ("CloseDoor");
+		}
+		doorTriggered = false;
 	}
 
 }
