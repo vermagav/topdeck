b8d65ed665c6f327631586a595c53017f84e513e
Gav Verma <verma.gav@gmail.com>
2014-07-13 19:11:10 -0400

    Basic room1 implemented under /Final/

---
+++ Final/Assets/Scripts/Level/ComputerPanel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComputerPanel : MonoBehaviour {
+
+	private bool powerState = false;
+	private bool isActive = false;
+
+	public void PowerOn() {
+		powerState = true;
+		// TODO: power on sounds and effects, need to give feedback to player
+		Debug.Log ("Computer Panel powered on...");
+	}
+
+	void Activate() {
+		if(isActive) {
+			return;
+		}
+		
+		// Open Door
+		// TODO
+		Debug.Log ("Computer Panel activated!");
+
+		isActive = true;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(powerState && other.tag == "Player") {
+			Activate();
+		}
+	}
+}
---
+++ Final/Assets/Scripts/Level/PowerSwitch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerSwitch : MonoBehaviour {
+	
+	private bool isActive = false;
+
+	public Light light;
+	public float activeLightIntensity = 0.3f;
+
+	public ComputerPanel computerPanel;
+
+	void Activate() {
+		if(isActive) {
+			return;
+		}
+
+		// Increase light intensity
+		light.intensity = activeLightIntensity;
+
+		// Power on computer panel
+		computerPanel.PowerOn();
+
+		isActive = true;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			Activate();
+		}
+	}
+}
