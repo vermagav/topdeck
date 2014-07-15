5d224c2497669b534df32af59e31754330066f72
tlabean <labean112@gmail.com>
2014-07-15 00:44:59 -0400

    Created animated gears
    move the small gear to the space between the larger gears to  make the
    magic happen

---
+++ Final/Assets/Scripts/Level/GearAttach.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GearAttach : MonoBehaviour {
+
+	public GameObject targetGear;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.Equals(targetGear))
+		{
+			GetComponent<Animator>().SetBool("hasCenterGear", true);
+			other.gameObject.SetActive(false);
+		}
+	}
+}
