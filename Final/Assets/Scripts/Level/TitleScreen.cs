a90c6d8edcd0cd608a82a00db1e28464879e41ba
Gav Verma <verma.gav@gmail.com>
2014-07-17 06:41:07 -0400

    Placeholder title screen

---
+++ Final/Assets/Scripts/Level/TitleScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	private bool titlePlayed = false;
+
+	void OnTriggerEnter() {
+		if (titlePlayed == false) {
+			// Title code goes here
+			titlePlayed = true;
+		}
+	}
+}
