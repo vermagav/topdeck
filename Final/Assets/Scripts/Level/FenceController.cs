52a32fa3315ecdb11de4d7544e067844da1badcb
Gav Verma <verma.gav@gmail.com>
2014-07-17 02:09:16 -0400

    Added music and fence sounds

--- Final/Assets/Scripts/Level/FenceController.cs
+++ Final/Assets/Scripts/Level/FenceController.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class FenceController : MonoBehaviour {
 
 	private Animator animator;
 
-	void Awake()
-	{
+	public AudioClip fenceSound;
+
+	void Awake() {
 		animator = GetComponent<Animator> ();
 	}
 
-	public void setFenceUp(bool state)
-	{
+	public void setFenceUp(bool state) {
 		animator.SetBool("FenceUp", state);
+
+		AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo (0);
+		if(!audio.isPlaying && stateInfo.IsTag("Transition")) {
+			audio.clip = fenceSound;
+			audio.Play();
+		}
 	}
 }
