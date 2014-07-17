394af4ad020d589c2343fc17167bcd6da1fe2311
Gav Verma <verma.gav@gmail.com>
2014-07-17 00:07:59 -0400

    room2 alpha completed

--- Final/Assets/Scripts/Level/GearAttach.cs
+++ Final/Assets/Scripts/Level/GearAttach.cs
@@ -5,12 +5,15 @@ public class GearAttach : MonoBehaviour {
 
 	public GameObject targetGear;
 
+	public AnimateDoor animateDoor;
+
 	void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.Equals(targetGear))
 		{
 			GetComponent<Animator>().SetBool("hasCenterGear", true);
 			other.gameObject.SetActive(false);
+			animateDoor.OpenDoor ();
 		}
 	}
 }
