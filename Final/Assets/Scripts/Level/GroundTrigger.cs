f0ceef54a6003ed3d795b3e7eba0e35353bd9805
Gav Verma <verma.gav@gmail.com>
2014-07-16 21:56:26 -0400

    Added ground trigger in room 2

---
+++ Final/Assets/Scripts/Level/GroundTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundTrigger : MonoBehaviour {
+
+	public Texture buttonOff;
+	public Texture buttonOn;
+
+	public AnimateDoor animateDoor;
+
+	bool triggerActive = false;
+
+	void OnCollisionStay () {
+		// Change texture
+		renderer.material.mainTexture = buttonOn;
+
+		// Lower the fence
+		animateDoor.OpenDoor ();
+
+		triggerActive = true;
+	}
+
+	void OnCollisionExit () {
+		// Change texture
+		renderer.material.mainTexture = buttonOff;
+
+		// Raise the fence ヽ༼ຈلຈ༽ﾉ
+		animateDoor.CloseDoor ();
+
+		triggerActive = false;
+	}
+
+	public bool GetTriggerState() 
+	{
+		return triggerActive;
+	}
+}
---
+++ Final/Assets/Scripts/Level/GroundTriggerSounds.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(GroundTrigger))]
+[RequireComponent (typeof(AudioSource))]
+public class GroundTriggerSounds : MonoBehaviour {
+
+	public AudioClip sfxActivated;
+	public AudioClip sfxDeactivated;
+
+	GroundTrigger trigger;
+	bool lastTriggerState;
+
+	// Use this for initialization
+	void Start () {
+		trigger = GetComponent<GroundTrigger>();
+		lastTriggerState = trigger.GetTriggerState();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastTriggerState != trigger.GetTriggerState())
+		{
+			lastTriggerState = trigger.GetTriggerState();
+			if (lastTriggerState)
+			{
+				//play open
+				if (sfxActivated != null)
+				{
+					audio.clip = sfxActivated;
+				}
+			}
+			else
+			{
+				//play close
+				if (sfxDeactivated != null)
+				{
+					audio.clip = sfxDeactivated;
+				}
+			}
+			audio.Play ();
+		}
+	
+	}
+}
