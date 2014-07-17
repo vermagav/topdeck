fcc1ff2688f7beb7725c1158ed636c87a6b7188a
tlabean <labean112@gmail.com>
2014-07-17 04:28:05 -0400

    added more sounds to room one and 2

--- Final/Assets/Scripts/Level/GearAttach.cs
+++ Final/Assets/Scripts/Level/GearAttach.cs
@@ -5,6 +5,8 @@ using System.Collections;
 public class GearAttach : MonoBehaviour {
 
 	public GameObject targetGear;
+	public AudioClip attachClip;
+	public AudioClip happyRobot;
 
 	public AnimateDoor animateDoor;
 
@@ -15,6 +17,8 @@ public class GearAttach : MonoBehaviour {
 			GetComponent<Animator>().SetBool("hasCenterGear", true);
 			other.gameObject.SetActive(false);
 			animateDoor.OpenDoor ();
+			AudioSource.PlayClipAtPoint(attachClip, transform.position);
+			AudioSource.PlayClipAtPoint(happyRobot, transform.position);
 		}
 	}
 }
