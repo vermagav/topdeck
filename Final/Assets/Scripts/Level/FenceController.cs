68828e9cb03faa26fee3a114f2cd832eff3dcaa4
tlabean <labean112@gmail.com>
2014-07-16 21:55:56 -0400

    Added animated fence

---
+++ Final/Assets/Scripts/Level/FenceController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FenceController : MonoBehaviour {
+
+	private Animator animator;
+
+	void Awake()
+	{
+		animator = GetComponent<Animator> ();
+	}
+
+	public void setFenceUp(bool state)
+	{
+		animator.SetBool("FenceUp", state);
+	}
+}
